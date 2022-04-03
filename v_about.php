6df4898dd0794808e95a95fd09d195f28a8e3c5d
MughniRahmatAlaziz <101989540+MughniRahmatAlaziz@users.noreply.github.com>
2022-04-03 21:41:16 +0700

    Tugas 3
    Tugas III Web Programming II
    Mecancang Tamplate Sederhana

---
+++ v_about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/css/stylebuku.css">
+    <title> Web Programming II</title>
+</head>
+<body>
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Mughni Rahmat</li>
+        <li>Nama Belakang : Alaziz</li>
+    </ul>
+    
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Kp.Cijaksa I 018/006 Desa.Padajaya Kec.Jampangkulon</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Sukabumi</li>
+    </ul>
+    
+    <h4>Olahraga Favorit</h4>
+    <ul type="Squre">
+        <li>Renang</li>
+        <li>Lari</li>
+    </ul>
+</section>
+</body>
+</html>
+    
---
+++ v_footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Web Programming Sederhana</title>
+</head>
+<body>
+    <div>
+        <footer>
+            <a href=”http://www.RentalBuku.com”>RentalBuku</a>
+        </footer>
+    </div>
+</body>
+</html>
